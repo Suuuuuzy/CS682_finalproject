b1bab0b8c8dfa03316d64f77138d230ebccd50d9
SuzyYu <zjyujianjia@163.com>
2021-05-05 01:10:59 +0800

    update the plot_data.py

--- plot_data.py
+++ plot_data.py
@@ -2,30 +2,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-
-# np.savez(train_perc_store=train_perc_store,  loss_batch_store=loss_batch_store, test_perc_store=test_perc_store, test_loss_store=test_loss_store)
+# np.savez(train_losses=train_losses,train_top1s=train_top1s,train_top5s=train_top5s, test_losses=test_losses,test_top1s=test_top1s, test_top5s=test_top5s)
 def plot_data(filepath):
     data = np.load(filepath)
 
-    if 'train_perc' in data:
-        plt.plot(data['train_perc'], '-o', label = 'train precision')
+    if 'train_top1s' in data:
+        plt.plot(data['train_top1s'], '-o', label = 'train top1 precision')
         plt.xlabel('epoch Number')
-    if 'test_perc' in data:
-        plt.plot(data['test_perc'], '-o', label = 'validation precision')
+    if 'test_top1s' in data:
+        plt.plot(data['test_top1s'], '-o', label = 'validation top1 precision')
         plt.xlabel('epoch Number')
-    plt.ylabel('train/val precision at each epoch')
+    plt.ylabel('Train/val top1 precision at each epoch')
     plt.legend()
-    plt.show()
+    plt.savefig('Train/val top1 precision at each epoch')
+    # plt.show()
 
-    if 'train_loss' in data:
-        plt.plot(data['train_loss'], '-o', label = 'train loss')
+    if 'train_top5s' in data:
+        plt.plot(data['train_top5s'], '-o', label = 'train top5 precision')
         plt.xlabel('epoch Number')
-    if 'test_loss' in data:
-        plt.plot(data['test_loss'], '-o', label = 'validation loss')
+    if 'test_top5s' in data:
+        plt.plot(data['test_top5s'], '-o', label = 'validation top5 precision')
         plt.xlabel('epoch Number')
-    plt.ylabel('train/val loss at each epoch')
+    plt.ylabel('Train/val top5 precision at each epoch')
     plt.legend()
-    plt.show()
+    plt.savefig('Train/val top1 precision at each epoch')
+    # plt.show()
+
+    if 'train_losses' in data:
+        plt.plot(data['train_losses'], '-o', label = 'train loss')
+        plt.xlabel('epoch Number')
+    if 'test_losses' in data:
+        plt.plot(data['test_losses'], '-o', label = 'validation loss')
+        plt.xlabel('epoch Number')
+    plt.ylabel('Train/val loss at each epoch')
+    plt.legend()
+    plt.savefig('Train/val top1 precision at each epoch')
+    # plt.show()
 
 
 
