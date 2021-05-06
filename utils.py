9853ba8fa43c8504cfb90a54f17525d9c6de9026
SuzyYu <zjyujianjia@163.com>
2021-05-07 00:16:18 +0800

    modify polylr

--- utils.py
+++ utils.py
@@ -200,7 +200,11 @@ class PolyLR(_LRScheduler):
         super(PolyLR, self).__init__(optimizer, last_epoch)
     
     def get_lr(self):
-        return [ max( base_lr * ( 1 - self.last_epoch/self.max_iters )**self.power, self.min_lr)
-                for base_lr in self.base_lrs]
+        lrs = []
+        for base_lr in self.base_lrs:
+          lrs.append(max( base_lr * ( 1 - self.last_epoch/self.max_iters )**self.power, self.min_lr))
+        return lrs
+        # return [ max( base_lr * ( 1 - self.last_epoch/self.max_iters )**self.power, self.min_lr)
+                # for base_lr in self.base_lrs]
 
 
