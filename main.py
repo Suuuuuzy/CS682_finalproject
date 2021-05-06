cb3df44cda5ef655f18eedaf36cbbde44e8439b9
SuzyYu <zjyujianjia@163.com>
2021-05-06 15:27:08 +0800

    add support for pretrain and finetune

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ parser.add_argument('--dataset', default='tiny-imagenet-200-01', help='TinyImage
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful to restarts)')
 parser.add_argument('--mode', default='baseline_train', help='baseline_train/pretrain/finetune')
+parser.add_argument('--pretrained_model', default='', type=str, metavar='PATH', help='path to the pretrained model')
 parser.add_argument('--epochs', default=10, type=int, metavar='N',
                     help='numer of total epochs to run')
 parser.add_argument('-b', '--batch_size', default=256, type=int, metavar='N',
@@ -37,7 +38,7 @@ parser.add_argument('--resume', default='', type=str, metavar='PATH',
                     help='path to latest checkpoitn, (default: None)')
 parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
                     help='evaluate model on validation set')
-parser.add_argument('-c', '--color_distortion', dest='color_distortion', action='store_true',
+parser.add_argument('-c', '--color_distortion', dest='color_distortion',default=True, type=bool,
                     help='add color_distortion to train set')
 parser.add_argument('--print_freq', '-f', default=40, type=int, metavar='N',
                     help='print frequency (default: 40)')
@@ -52,19 +53,41 @@ def main():
     # model
     if args.mode=='baseline_train':
         model = initialize_model(use_resnet=True, pretrained=False, nclasses=200)
+        criterion = nn.CrossEntropyLoss()
+    elif args.mode=='pretrain':
+        model = initialize_model(use_resnet=True, pretrained=False, nclasses=3)
+        criterion = nn.MSELoss()
+    elif args.mode=='finetune':
+        model = initialize_model(use_resnet=True, pretrained=False, nclasses=3)
+        criterion = nn.CrossEntropyLoss()
+        # load the pretrained model
+        if args.pretrained_model:
+            if os.path.isfile(args.pretrained_model):
+                print("=> loading pretrained model '{}'".format(args.pretrained_model))
+                checkpoint = torch.load(args.pretrained_model)
+                args.start_epoch = checkpoint['epoch']
+                best_prec1 = checkpoint['best_prec1']
+                model.load_state_dict(checkpoint['state_dict'])
+                optimizer.load_state_dict(checkpoint['optimizer'])
+                print("=> loaded pretrained model '{}' (epoch {})".format(args.resume, checkpoint['epoch']))
+
+
     if torch.cuda.is_available:
         model = model.cuda()
     optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
     train_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.step_size, gamma=args.gamma) 
-    criterion = nn.CrossEntropyLoss()
+    
+    if args.mode in ['baseline_train', 'finetune']:
+        train_losses = []
+        train_top1s = []
+        train_top5s = []
 
-    train_losses = []
-    train_top1s = []
-    train_top5s = []
-
-    test_losses = []
-    test_top1s = []
-    test_top5s = []
+        test_losses = []
+        test_top1s = []
+        test_top5s = []
+    elif args.mode == 'pretrain':
+        train_losses = []
+        test_losses = []
 
     # optionlly resume from a checkpoint
     if args.resume:
@@ -78,67 +101,109 @@ def main():
             optimizer.load_state_dict(checkpoint['optimizer'])
             datafile = args.resume.split('.pth')[0] + '.npz'
             load_data = np.load(datafile)
-            train_losses = list(load_data['train_losses'])
-            train_top1s = list(load_data['train_top1s'])
-            train_top5s = list(load_data['train_top5s'])
-            test_losses = list(load_data['test_losses'])
-            test_top1s = list(load_data['test_top1s'])
-            test_top5s = list(load_data['test_top5s'])
+            if args.mode in ['baseline_train', 'finetune']:
+                train_losses = list(load_data['train_losses'])
+                train_top1s = list(load_data['train_top1s'])
+                train_top5s = list(load_data['train_top5s'])
+                test_losses = list(load_data['test_losses'])
+                test_top1s = list(load_data['test_top1s'])
+                test_top5s = list(load_data['test_top5s'])
+            elif args.mode=='pretrain':
+                train_losses = list(load_data['train_losses'])
+                test_losses = list(load_data['test_losses'])
             print("=> loaded checkpoint '{}' (epoch {})".format(args.resume, checkpoint['epoch']))
         else:
             print("=> no checkpoint found at '{}'".format(args.resume))
 
-    # data
-    # if args.dataset=='TinyImageNet':
-    from utils import TinyImageNet_data_loader
-    train_loader, val_loader = TinyImageNet_data_loader(args.dataset, args.batch_size,color_distortion=args.color_distortion)
-    
-    # if evaluate the model
-    if args.evaluate:
-        print('evaluate this model on validation dataset')
-        validate(val_loader, model, criterion, args.print_freq)
-        return
-    
+    if args.mode in ['baseline_train', 'finetune']:
+        # data
+        from utils import TinyImageNet_data_loader
+        train_loader, val_loader = TinyImageNet_data_loader(args.dataset, args.batch_size,color_distortion=args.color_distortion)
+        
+        # if evaluate the model
+        if args.evaluate:
+            print('evaluate this model on validation dataset')
+            validate(val_loader, model, criterion, args.print_freq)
+            return
+        
+        for epoch in range(args.start_epoch, args.epochs):
+            adjust_learning_rate(optimizer, epoch, args.lr)
+            time1 = time.time() #timekeeping
 
-    for epoch in range(args.start_epoch, args.epochs):
-        adjust_learning_rate(optimizer, epoch, args.lr)
-        time1 = time.time() #timekeeping
+            # train for one epoch
+            loss, top1, top5 = train(train_loader, model, criterion, optimizer, epoch, args.print_freq)
+            train_losses.append(loss)
+            train_top1s.append(top1)
+            train_top5s.append(top5)
 
-        # train for one epoch
-        loss, top1, top5 = train(train_loader, model, criterion, optimizer, epoch, args.print_freq)
-        train_losses.append(loss)
-        train_top1s.append(top1)
-        train_top5s.append(top5)
+            # evaluate on validation set
+            loss, prec1, prec5 = validate(val_loader, model, criterion, args.print_freq)
+            test_losses.append(loss)
+            test_top1s.append(prec1)
+            test_top5s.append(prec5)
 
-        # evaluate on validation set
-        loss, prec1, prec5 = validate(val_loader, model, criterion, args.print_freq)
-        test_losses.append(loss)
-        test_top1s.append(prec1)
-        test_top5s.append(prec5)
+            # remember the best prec@1 and save checkpoint
+            is_best = prec1 > best_prec1
+            best_prec1 = max(prec1, best_prec1)
 
-        # remember the best prec@1 and save checkpoint
-        is_best = prec1 > best_prec1
-        best_prec1 = max(prec1, best_prec1)
+            save_checkpoint({
+                'epoch': epoch + 1,
+                'mode': args.mode,
+                'state_dict': model.state_dict(),
+                'best_prec1': best_prec1,
+                'optimizer': optimizer.state_dict()
+            }, is_best, args.mode + '_' + args.dataset +'.pth')
 
-        save_checkpoint({
-            'epoch': epoch + 1,
-            'mode': args.mode,
-            'state_dict': model.state_dict(),
-            'best_prec1': best_prec1,
-            'optimizer': optimizer.state_dict()
-        }, is_best, args.mode + '_' + args.dataset +'.pth')
+            np.savez(args.mode + '_' + args.dataset +'.npz', train_losses=train_losses,train_top1s=train_top1s,train_top5s=train_top5s, test_losses=test_losses,test_top1s=test_top1s, test_top5s=test_top5s)
+            # train_scheduler.step()
+           # np.savez(args.mode + '_' + args.dataset +'.npz', train_losses=train_losses)
+            time2 = time.time() #timekeeping
+            print('Elapsed time for epoch:',time2 - time1,'s')
+            print('ETA of completion:',(time2 - time1)*(args.epochs - epoch - 1)/60,'minutes')
+            print()
+    elif args.mode=='pretrain':
+        #data
+        from utils import TinyImageNet_data_loader
+        train_loader, val_loader = TinyImageNet_data_loader(args.dataset, args.batch_size,color_distortion=args.color_distortion)
+        
+        # if evaluate the model, show some results
+        if args.evaluate:
+            print('evaluate this model on validation dataset')
+            validate(val_loader, model, criterion, args.print_freq)
+            return
 
-        np.savez(args.mode + '_' + args.dataset +'.npz', train_losses=train_losses,train_top1s=train_top1s,train_top5s=train_top5s, test_losses=test_losses,test_top1s=test_top1s, test_top5s=test_top5s)
-        # train_scheduler.step()
-       # np.savez(args.mode + '_' + args.dataset +'.npz', train_losses=train_losses)
-        time2 = time.time() #timekeeping
-        print('Elapsed time for epoch:',time2 - time1,'s')
-        print('ETA of completion:',(time2 - time1)*(args.epochs - epoch - 1)/60,'minutes')
-        print()
+        for epoch in range(args.start_epoch, args.epochs):
+            adjust_learning_rate(optimizer, epoch, args.lr)
+            time1 = time.time() #timekeeping
+
+            # train for one epoch
+            loss, _, _ = train(train_loader, model, criterion, optimizer, epoch, args.print_freq, colorization=True)
+            train_losses.append(loss)
+
+            # evaluate on validation set
+            loss, _, _ = validate(val_loader, model, criterion, args.print_freq, colorization=True)
+            test_losses.append(loss)
+
+            save_checkpoint({
+                'epoch': epoch + 1,
+                'mode': args.mode,
+                'state_dict': model.state_dict(),
+                'best_prec1': 0,
+                'optimizer': optimizer.state_dict()
+            }, is_best, args.mode + '_' + args.dataset +'.pth')
+
+            np.savez(args.mode + '_' + args.dataset +'.npz', train_losses=train_losses, test_losses=test_losses)
+            # train_scheduler.step()
+           # np.savez(args.mode + '_' + args.dataset +'.npz', train_losses=train_losses)
+            time2 = time.time() #timekeeping
+            print('Elapsed time for epoch:',time2 - time1,'s')
+            print('ETA of completion:',(time2 - time1)*(args.epochs - epoch - 1)/60,'minutes')
+            print()
 
 
 
-def train(train_loader, model, criterion, optimizer, epoch, print_freq):
+
+def train(train_loader, model, criterion, optimizer, epoch, print_freq, colorization=False):
     batch_time = AverageMeter()
     data_time = AverageMeter()
     losses = AverageMeter()
@@ -159,12 +224,12 @@ def train(train_loader, model, criterion, optimizer, epoch, print_freq):
         # compute output
         output = model(input)
         loss = criterion(output, target)
-
         # measure accuracy and record loss
-        prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
         losses.update(loss.item(), input.size(0))
-        top1.update(prec1, input.size(0))
-        top5.update(prec5, input.size(0))
+        if not colorization:
+            prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
+            top1.update(prec1, input.size(0))
+            top5.update(prec5, input.size(0))
 
         # compute gradient and do SGD step
         optimizer.zero_grad()
@@ -176,20 +241,28 @@ def train(train_loader, model, criterion, optimizer, epoch, print_freq):
         end = time.time()
 
         if i % print_freq == 0:
-            print('Epoch: [{0}][{1}/{2}]\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})\t'.format(
-                epoch, i, len(train_loader), batch_time=batch_time,
-                data_time=data_time, loss=losses, top1=top1, top5=top5))
+            if not colorization:
+                print('Epoch: [{0}][{1}/{2}]\t'
+                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                      'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                      'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                      'Prec@5 {top5.val:.3f} ({top5.avg:.3f})\t'.format(
+                    epoch, i, len(train_loader), batch_time=batch_time,
+                    data_time=data_time, loss=losses, top1=top1, top5=top5))
+            else:
+                print('Epoch: [{0}][{1}/{2}]\t'
+                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                      'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(
+                    epoch, i, len(train_loader), batch_time=batch_time,
+                    data_time=data_time, loss=losses))
 
     # return loss, top1, top5 corresponding to each epoch
     return losses.avg, top1.avg, top5.avg
 
 
-def validate(val_loader, model, criterion, print_freq):
+def validate(val_loader, model, criterion, print_freq, colorization=False):
     batch_time = AverageMeter()
     losses = AverageMeter()
     top1 = AverageMeter()
@@ -206,25 +279,29 @@ def validate(val_loader, model, criterion, print_freq):
             # compute output
             output = model(input)
             loss = criterion(output, target)
-
             # measure accuracy and record loss
-            prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
             losses.update(loss.item(), input.size(0))
-            top1.update(prec1, input.size(0))
-            top5.update(prec5, input.size(0))
+            if not colorization:
+                prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
+                top1.update(prec1, input.size(0))
+                top5.update(prec5, input.size(0))
 
             # measure elapsed time
             batch_time.update(time.time() - end)
             end = time.time()
-
-    print('Test: [{0}/{1}]\t'
-          'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-          'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-          'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-          'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-        i, len(val_loader), batch_time=batch_time, loss=losses,
-        top1=top1, top5=top5))
-
+    if not colorization:
+        print('Test: [{0}/{1}]\t'
+              'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+              'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+              'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+              'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+            i, len(val_loader), batch_time=batch_time, loss=losses,
+            top1=top1, top5=top5))
+    else:
+        print('Test: [{0}/{1}]\t'
+              'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+              'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(
+            i, len(val_loader), batch_time=batch_time, loss=losses))
     return losses.avg, top1.avg, top5.avg
 
 
