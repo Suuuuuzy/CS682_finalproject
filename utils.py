557d2bdf884147506f4a3ae273f9de901f48dec7
Suuuuuzy <yujianjiasuzy@gmail.com>
2021-05-06 04:07:06 +0000

    debug from server

--- utils.py
+++ utils.py
@@ -118,10 +118,10 @@ def make_dataset(root, base_folder, dirname, class_to_idx):
 
 def get_color_distortion(s=1.0):
     # s is the strength of color distortion.
-    color_jitter = T.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
-    rnd_color_jitter = T.RandomApply([color_jitter], p=0.8)
-    rnd_gray = T.RandomGrayscale(p=0.2)
-    color_distort = T.Compose([rnd_color_jitter, rnd_gray])
+    color_jitter = transforms.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
+    rnd_color_jitter = transforms.RandomApply([color_jitter], p=0.8)
+    rnd_gray = transforms.RandomGrayscale(p=0.2)
+    color_distort = transforms.Compose([rnd_color_jitter, rnd_gray])
     return color_distort
 
 
